4754def9dea747bfc62635184f0fd35ba06edf03
unknown <kalyansingh277@gmail.com>
2020-09-23 00:09:17 +0530

    first commit

---
+++ src/components/NavWrapper.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    text-transform: capitalize;
+    font-size: 1.3rem !important;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { ProductProvider } from './components/context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductProvider>
+      <Router>
+        <App />
+      </Router>
+    </ProductProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
